e1136762e8065a744b34aa571e1b415ec58b87f2
elsiosanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-03-13 09:33:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-qt9pxa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-qt9pxa)
---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <h1>
+     {{ 'alo' }}
+     <ADButton />
+    </h1>
+  </div>
+</template>
